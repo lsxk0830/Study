75a15c2f5455ea2786fcc821935f2d774c7bd45f
蓝色星空 <lsxk0830@163.com>
2024-09-20 16:24:03 +0800

    冒泡排序

---
+++ Unity/Assets/ToolModule/排序/冒泡排序.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tool
+{
+	/// <summary>
+	/// 依次比较相邻的两个元素，进行交换
+	/// 循环冒泡，次数为数据个数-1
+	/// </summary>
+	public class 冒泡排序 : MonoBehaviour
+	{
+		private List<int> numList = new List<int>() { 9, 7, 8, 6, 5, 4, 3, 2, 1, 0 };
+		private int count = 0;
+
+		void Start()
+		{
+			Sort();
+
+			string sortStr = "";
+			for (int i = 0; i < numList.Count; i++)
+			{
+				sortStr += numList[i] + "、";
+			}
+			Debug.Log($"默认次数：{count}。{sortStr}");
+		}
+
+		private void Sort()
+		{
+			for (int i = 0; i < numList.Count - 1; i++)
+			{
+				for (int j = 0; j < numList.Count - 1 - i; j++)
+				{
+					if (numList[j] < numList[j + 1]) //相邻的两个数，前面的值比后面的大，则两两交换值
+					{
+						int temp = numList[j];
+						numList[j] = numList[j + 1];
+						numList[j + 1] = temp;
+					}
+					count++;
+				}
+			}
+		}
+	}
+}
---
+++ Unity/Assets/ToolModule/排序/冒泡排序改进_A.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tool
+{
+	/// <summary>
+	/// 【场景分析】
+	///  在某次遍历中如果没有数据交换，说明整个数组已经有序。
+	///  若初始序列就是排序好的，如果用基础的冒泡排序方法，仍然还要比较O(N^2)次，但无交换次数。
+	///  【改进思路】
+	///  通过设置标志位来记录此次遍历有无数据交换，进而可以判断是否要继续循环，设置一个flag标记，
+	///  当在一趟序列中没有发生交换，则该序列已排序好，但优化后排序的时间复杂度没有发生量级的改变
+	/// </summary>
+	public class 冒泡排序改进_A : MonoBehaviour
+	{
+		private List<int> numList = new List<int>() { 9, 7, 8, 6, 5, 4, 3, 2, 1, 0 };
+		private int count = 0;
+
+		void Start()
+		{
+			Sort();
+
+			string sortStr = "";
+			for (int i = 0; i < numList.Count; i++)
+			{
+				sortStr += numList[i] + "、";
+			}
+			Debug.Log($"改进A次数:{count}。{sortStr}");
+		}
+
+		private void Sort()
+		{
+			for (int i = 0; i < numList.Count - 1; i++)
+			{
+				bool exchange = true;  //冒泡的改进，若在一趟中没有发生逆序，则该序列已有序
+
+				for (int j = 0; j < numList.Count - 1 - i; j++)
+				{
+					if (numList[j] < numList[j + 1])
+					{
+						int temp = numList[j];
+						numList[j] = numList[j + 1];
+						numList[j + 1] = temp;
+
+						exchange = false;
+					}
+					count++;
+				}
+				if (exchange)
+				{
+					return;
+				}
+			}
+		}
+	}
+}
