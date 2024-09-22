7f1065432c1f7d9cbccd2adde699fe716e166c77
蓝色星空 <lsxk0830@163.com>
2024-09-22 11:10:21 +0800

    排序更新

---
+++ Unity/Assets/ToolModule/排序/插入排序_二分插入.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tool
+{
+    /// <summary>
+    /// 【实现逻辑】
+    /// 1. 从第一个元素开始，该元素可以认为已经被排序
+    /// 2. 取出下一个元素，在已经排序的元素序列中二分查找到第一个比它大的数的位置
+    /// 3. 将新元素插入到该位置后
+    /// 4. 重复上述两步
+    /// </summary>
+    public class 插入排序_二分插入 : MonoBehaviour
+    {
+        private List<int> numList = new List<int>() { 1, 5, 6, 5, 8, 2, 1, 3, 4, 9 };
+        void Start()
+        {
+            Sort();
+
+            string sortStr = "";
+            for (int i = 0; i < numList.Count; i++)
+            {
+                sortStr += numList[i] + "、";
+            }
+            Debug.Log($"插入排序_二分插入:{sortStr}");
+        }
+
+        private void Sort()
+        {
+            int key, left, right, middle;
+            for (int i = 1; i < numList.Count; i++)
+            {
+                key = numList[i];
+                left = 0;
+                right = i - 1;
+                while (left <= right)
+                {
+                    middle = (left + right) / 2;
+                    if (numList[middle] > key)
+                        right = middle - 1;
+                    else
+                        left = middle + 1;
+                }
+
+                for (int j = i - 1; j >= left; j--)
+                {
+                    numList[j + 1] = numList[j];
+                }
+
+                numList[left] = key;
+            }
+        }
+    }
+}
---
+++ Unity/Assets/ToolModule/排序/选择排序.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tool
+{
+    /// <summary>
+    /// 【实现逻辑】
+    /// 1. 第一轮从下标为 1 到下标为 n-1 的元素中选取最小值，若小于第一个数，则交换
+    /// 2. 第二轮从下标为 2 到下标为 n-1 的元素中选取最小值，若小于第二个数，则交换
+    /// 3. 依次类推下去…
+    /// </summary>
+    public class 选择排序 : MonoBehaviour
+    {
+        private List<int> numList = new List<int>() { 1, 5, 6, 5, 8, 2, 1, 3, 4, 9 };
+
+        void Start()
+        {
+            Sort();
+
+            string sortStr = "";
+            for (int i = 0; i < numList.Count; i++)
+            {
+                sortStr += numList[i] + "、";
+            }
+            Debug.Log($"默认次数：{sortStr}");
+        }
+
+        private void Sort()
+        {
+            for (int i = 0; i < numList.Count - 1; i++)
+            {
+                int min = i;
+                //每轮需要比较的次数n-i
+                for (int j = i + 1; j < numList.Count; j++)
+                {
+                    if (numList[min] > numList[j]) // 当此数大于后一个数
+                    {
+                        // 记录目前能找到的最小值元素的下标
+                        min = j;
+                    }
+                }
+                int temp = numList[i];
+                numList[i] = numList[min];
+                numList[min] = temp;
+            }
+        }
+    }
+}
