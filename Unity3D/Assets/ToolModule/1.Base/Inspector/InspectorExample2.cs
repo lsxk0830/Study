08fc62f8085702b5bb496b14c753b30ea66c38ee
蓝色星空 <lsxk0830@163.com>
2025-03-31 23:04:51 +0800

    更新

--- Unity3D/Assets/ToolModule/1.Base/Inspector/InspectorExample2.cs
+++ Unity3D/Assets/ToolModule/1.Base/Inspector/InspectorExample2.cs
@@ -1,15 +1,24 @@
 using SimpleFrame;
 using UnityEngine;
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
 
 namespace Unity3D.Base.InspectorExample
 {
     public class InspectorExample2 : MonoBehaviour
     {
+        [Header("这个不能放私有的上面,序列化了都不行")]
         [LabelText("数量1")] public int Count1;
+
         [LabelText("数量2")] private int Count2;
         [SerializeField][LabelText("数量3")] private int Count3;
+
+        public Type myType;
+    }
+
+    public enum Type
+    {
+        [InspectorName("第一")]
+        One,
+        [InspectorName("第二")]
+        Two
     }
 }
---
+++ Unity3D/Assets/ToolModule/1.Base/Inspector/InspectorExample4.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Unity3D.Base.InspectorExample
+{
+    /// <summary>
+    /// 必须绑定BoxCollider组件
+    /// </summary>
+    [RequireComponent(typeof(BoxCollider))]
+    public class InspectorExample4 : MonoBehaviour
+    {
+
+    }
+}
