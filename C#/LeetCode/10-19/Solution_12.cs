72eb52f69931c655b11977457e9451eee4f2716c
蓝色星空 <lsxk0830@163.com>
2025-04-02 17:08:03 +0800

    预定义符号表和贪心算法

--- C#/LeetCode/10-19/Solution_12.cs
+++ C#/LeetCode/10-19/Solution_12.cs
@@ -18,125 +18,30 @@ namespace LeetCode
     {
         public void Excute()
         {
-            int num = 10;
+            int num = 3749;
             string s = IntToRoman(num);
             Console.WriteLine($"整数转罗马数字：{s}");
         }
 
         public string IntToRoman(int num)
         {
-            StringBuilder stringBuilder = new StringBuilder();
-            // 四类情况
-            while (num > 0)
+            (int Value, string Symbol)[] _romanSymbols =
             {
-                if (num >= 1000)
+                (1000, "M"),  (900, "CM"), (500, "D"), (400, "CD"),
+                (100, "C"),   (90, "XC"),  (50, "L"),  (40, "XL"),
+                (10, "X"),    (9, "IX"),   (5, "V"),   (4, "IV"),
+                (1, "I")
+            };
+            StringBuilder sb = new StringBuilder();
+            foreach (var pair in _romanSymbols)
+            {
+                while (num >= pair.Value)
                 {
-                    num -= General(num,1000, stringBuilder);
-                }
-                else if (num >= 100)
-                {
-                    num -= General(num, 100, stringBuilder);
-                }
-                else if (num >= 10)
-                {
-                    num -= General(num, 10, stringBuilder);
-                }
-                else
-                {
-                    num -= General(num, 1, stringBuilder);
+                    sb.Append(pair.Symbol);
+                    num -= pair.Value;
                 }
             }
-            return stringBuilder.ToString();
-        }
-
-        private int General(int num,int s, StringBuilder stringBuilder)
-        {
-            int i = num / s;
-            if (i == 4 || i == 9)
-                stringBuilder.Append(Other(i * s));
-            else if (i < 5)
-            {
-                stringBuilder.Append(new string(Change(s), i));
-            }
-            else if (i == 5)
-            {
-                stringBuilder.Append(Change(5*s));
-            }
-            else
-            {
-                stringBuilder.Append(Change(5*s));
-                stringBuilder.Append(new string(Change(s), i - 5));
-            }
-            return i * s;
-        }
-
-        private char Change(int s)
-        {
-            char c;
-            switch (s)
-            {
-                case 1:
-                    c = 'I';
-                    break;
-                case 5:
-                    c = 'V';
-                    break;
-                case 10:
-                    c = 'X';
-                    break;
-                case 50:
-                    c = 'L';
-                    break;
-                case 100:
-                    c = 'C';
-                    break;
-                case 500:
-                    c = 'D';
-                    break;
-                case 1000:
-                    c = 'M';
-                    break;
-                default:
-                    c = 'I';
-                    break;
-            }
-            return c;
-        }
-
-        private string Other(int s)
-        {
-            string str;
-            switch (s)
-            {
-                case 4:
-                    str = "IV";
-                    break;
-                case 40:
-                    str = "XL";
-                    break;
-                case 400:
-                    str = "CD";
-                    break;
-                case 4000:
-                    str = "MMMM";
-                    break;
-                case 9:
-                    str = "IX";
-                    break;
-                case 90:
-                    str = "XC";
-                    break;
-                case 900:
-                    str = "CM";
-                    break;
-                case 9000:
-                    str = "MMMMMMMMM";
-                    break;
-                default:
-                    str = "";
-                    break;
-            }
-            return str;
+            return sb.ToString();
         }
     }
 }
