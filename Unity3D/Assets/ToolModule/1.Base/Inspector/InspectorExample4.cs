3d05ae47a9ae1f4379c4a9c9ac312e2635c0a73b
蓝色星空 <lsxk0830@163.com>
2025-04-03 22:17:30 +0800

    Update InspectorExample4.cs

--- Unity3D/Assets/ToolModule/1.Base/Inspector/InspectorExample4.cs
+++ Unity3D/Assets/ToolModule/1.Base/Inspector/InspectorExample4.cs
@@ -1,3 +1,5 @@
+using System;
+using SimpleFrame;
 using UnityEngine;
 
 namespace Unity3D.Base.InspectorExample
@@ -8,6 +10,12 @@ namespace Unity3D.Base.InspectorExample
     [RequireComponent(typeof(BoxCollider))]
     public class InspectorExample4 : MonoBehaviour
     {
+        [TextArea(5, 10)] public String myStr;
+        [LabelText("限制Int的范围")][Range(5, 10)] public int myInt;
+        [LabelText("限制float的范围")][Range(5, 10)] public float myfloat;
+        [Tooltip("鼠标悬浮提示")] public String tip;
 
+        [LabelText("没有半透明的Color")][ColorUsage(false)] public Color color;
+        [LabelText("半透明的Color")] public Color color2;
     }
 }
